82fa0a5d899f9c6da1eaf3ca306d8f3e408b7de8
Ursu17 <138593499+Ursu17@users.noreply.github.com>
2023-07-04 17:32:50 +0300

    Update __init__.py

---
+++ joinleave/__init__.py
@@ -0,0 +1,11 @@
+from .joinleave import JoinLeave
+from pathlib import Path
+import json
+
+with open(Path(__file__).parent / "info.json") as fp:
+    __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
+
+
+async def setup(bot):
+    cog = JoinLeave(bot)
+    await bot.add_cog(cog)
