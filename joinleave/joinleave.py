15d953ada620b57ec0e66fd20a814b2b5d370511
Ursu17 <138593499+Ursu17@users.noreply.github.com>
2023-07-04 19:34:07 +0300

    Update joinleave.py

--- joinleave/joinleave.py
+++ joinleave/joinleave.py
@@ -1,5 +1,5 @@
 import discord
-from datetime import datetime, timedelta, timezone
+import datetime
 from redbot.core import commands, Config
 from redbot.core.i18n import Translator, cog_i18n
 
@@ -13,49 +13,71 @@ class JoinLeave(commands.Cog):
         self.bot = bot
         self.config = Config.get_conf(self, identifier=1234567890)
         default_guild = {
-            "welcome_channel": None,
-            "farewell_channel": None,
+            "channel": None,
+            "join": True,
+            "leave": True
         }
         self.config.register_guild(**default_guild)
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        data = await self.config.guild(member.guild).all()
-        channel_id = data["welcome_channel"]
-        if channel_id is None:
+        join = await self.config.guild(member.guild).join()
+        if not join:
             return
-        channel = member.guild.get_channel(channel_id)
+        channel = member.guild.get_channel(
+            await self.config.guild(member.guild).channel()
+        )
         if not channel:
             return
-        timestamp = datetime.now(timezone.utc)
-        created_at = member.created_at.astimezone(timezone.utc)
-        days_since_creation = (timestamp - created_at).days
-        embed = discord.Embed(color=discord.Color.green())
-        embed.set_thumbnail(url=member.avatar.url)
-        embed.set_author(name=f"{member.name} a intrat pe serverul de Discord", icon_url=member.avatar.url)
-        embed.add_field(name="Membri:", value=f"{str(member.guild.member_count)} (acum {days_since_creation} zile)", inline=True)
-        embed.set_footer(text=f"Data și ora: {timestamp.strftime('%Y-%m-%d %H:%M:%S')} UTC")
+        time = datetime.datetime.utcnow()
+        users = len(member.guild.members)
+        since_created = (time - member.created_at).days
+        user_created = member.created_at.strftime("%Y-%m-%d, %H:%M")
+        created_on = f"{user_created} ({since_created} days ago)"
+
+        embed = discord.Embed(
+            description=f"{member.mention} ({member.name}#{member.discriminator})",
+            colour=discord.Colour.green(),
+            timestamp=member.joined_at,
+        )
+        embed.add_field(name="Total Membrii:", value=str(users))
+        embed.add_field(name="Cont creeat la:", value=created_on)
+        embed.set_footer(text=f"User ID: {member.id}")
+        embed.set_author(
+            name=f"{member.name} a intrat pe serverul de discord",
+            url=member.avatar_url,
+            icon_url=member.avatar_url,
+        )
+        embed.set_thumbnail(url=member.avatar_url)
         await channel.send(embed=embed)
 
     @commands.Cog.listener()
     async def on_member_remove(self, member):
-        data = await self.config.guild(member.guild).all()
-        channel_id = data["farewell_channel"]
-        if channel_id is None:
+        leave = await self.config.guild(member.guild).leave()
+        if not leave:
             return
-        channel = member.guild.get_channel(channel_id)
+        channel = member.guild.get_channel(
+            await self.config.guild(member.guild).channel()
+        )
         if not channel:
             return
-        timestamp = datetime.now(timezone.utc)
-        created_at = member.created_at.astimezone(timezone.utc)
-        days_since_creation = (timestamp - created_at).days
-        embed = discord.Embed(color=discord.Color.red())
-        embed.set_thumbnail(url=member.avatar.url)
-        embed.set_author(name=f"{member.name} a părăsit serverul de Discord", icon_url=member.avatar.url)
-        embed.add_field(name="Membri:", value=f"{str(member.guild.member_count)} (acum {days_since_creation} zile)", inline=True)
-        embed.set_footer(text=f"Data și ora: {timestamp.strftime('%Y-%m-%d %H:%M:%S')} UTC")
-        await channel.send(embed=embed)
+        time = datetime.datetime.utcnow()
+        users = len(member.guild.members)
 
+        embed = discord.Embed(
+            description=f"{member.mention} ({member.name}#{member.discriminator})",
+            colour=discord.Colour.red(),
+            timestamp=time,
+        )
+        embed.add_field(name="Total Membrii:", value=str(users))
+        embed.set_footer(text=f"User ID: {member.id}")
+        embed.set_author(
+            name=f"{member.name} a iesit de pe serverul de discord",
+            url=member.avatar_url,
+            icon_url=member.avatar_url,
+        )
+        embed.set_thumbnail(url=member.avatar_url)
+        await channel.send(embed=embed)
     @commands.group()
     async def joinleave(self, ctx):
         """Comenzi pentru evenimentele de intrare/ieșire"""
