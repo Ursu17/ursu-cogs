b2f198b96c2d589d3836983a79c4683abab44600
Ursu17 <138593499+Ursu17@users.noreply.github.com>
2023-08-30 17:58:47 +0300

    Update pontajinout.py

--- pontajinout/pontajinout.py
+++ pontajinout/pontajinout.py
@@ -13,8 +13,11 @@ class PontajInOut(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.config = Config.get_conf(self, identifier=1234567890)
-        default_guild = {}
-        self.config.register_user(**default_guild)
+        default_user = {
+            "in": None,
+            "out": None
+        }
+        self.config.register_user(**default_user)
         self.bucharest_tz = timezone('Europe/Bucharest')
 
     async def post_message(self, channel, content):
@@ -30,20 +33,25 @@ class PontajInOut(commands.Cog):
     async def pontaj_in(self, ctx):
         """Înregistrează pontajul de intrare"""
         user_pontaje = await self.config.user(ctx.author).all()
-        if "in" in user_pontaje:
+        if user_pontaje["in"] is not None:
             await ctx.send("Ai înregistrat deja un pontaj 'in'.")
             return
 
         user_pontaje["in"] = datetime.now(self.bucharest_tz).strftime('%H:%M')
         await self.config.user(ctx.author).set(user_pontaje)
         await ctx.message.delete()
+        await ctx.send(f"{ctx.author.mention} a intrat în tură la ora {user_pontaje['in']}")
 
     @pontaj.command(name="out")
     async def pontaj_out(self, ctx):
         """Înregistrează pontajul de ieșire și calculează durata de lucru"""
         user_pontaje = await self.config.user(ctx.author).all()
-        if "in" not in user_pontaje:
+        if user_pontaje["in"] is None:
             await ctx.send("Folosește mai întâi **!pontaj in**.")
+            return
+
+        if user_pontaje["out"] is not None:
+            await ctx.send("Ai înregistrat deja un pontaj 'out'.")
             return
 
         user_pontaje["out"] = datetime.now(self.bucharest_tz).strftime('%H:%M')
@@ -55,8 +63,8 @@ class PontajInOut(commands.Cog):
         work_duration = out_time - in_time
         work_minutes = int(work_duration.total_seconds() / 60)
 
-        await ctx.send(f"{ctx.author.mention} a ieșit din tură la ora **{out_time.strftime('%H:%M')}** "
-                       f"(A stat în tură **{work_minutes}** minute)")
+        await ctx.send(f"{ctx.author.mention} a ieșit din tură la ora {user_pontaje['out']} "
+                       f"(A stat în tură {work_minutes} de minute)")
 
 # Instanțiați și adăugați extensia la bot
 def setup(bot):
