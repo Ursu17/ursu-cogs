2bd344605a06e8474c04fd3ed8cb50dbe37704ba
Ursu17 <138593499+Ursu17@users.noreply.github.com>
2023-08-30 17:47:11 +0300

    Update pontajinout.py

--- pontajinout/pontajinout.py
+++ pontajinout/pontajinout.py
@@ -62,6 +62,45 @@ class PontajInOut(commands.Cog):
         except discord.NotFound:
             pass
 
+    @pontaj.command(name="out")
+    async def pontaj_out(self, ctx):
+        """Înregistrează pontajul de ieșire și calculează durata de lucru"""
+        guild_settings = await self.config.guild(ctx.guild).all()
+        user_pontaje = guild_settings["pontaje"].get(ctx.author.id, [])
+
+        if not user_pontaje:
+            await ctx.send("Folosește mai întâi **!pontaj in**.")
+            return
+        
+        pontaj_out_time = self.bucharest_tz.localize(datetime.now())
+        work_duration = pontaj_out_time - user_pontaje[-1]
+        work_minutes = int(work_duration.total_seconds() / 60)
+        user_pontaje.pop()  # Scoatem ultimul pontaj de intrare
+
+        await self.config.guild(ctx.guild).set_raw("pontaje", ctx.author.id, value=[dt.isoformat() for dt in user_pontaje])
+
+        try:
+            await ctx.message.delete()
+        except discord.NotFound:
+            pass
+        
+        guild_settings = await self.config.guild(ctx.guild).all()
+        pontaj_out_channel_id = guild_settings["pontaj_out_channel"]
+        pontaj_out_channel = ctx.guild.get_channel(pontaj_out_channel_id)
+
+        if pontaj_out_channel:
+            await self.post_message(pontaj_out_channel,
+                                    f"{ctx.author.mention} a ieșit din tură la ora {pontaj_out_time.strftime('%H:%M')} "
+                                    f"(A stat în tură {work_minutes} minute)")
+        else:
+            await ctx.send("Canalul pentru înregistrarea pontajului de ieșire nu este configurat sau nu există.")
+
+        await asyncio.sleep(3)
+        try:
+            await ctx.message.delete()
+        except discord.NotFound:
+            pass
+
     # Restul codului pentru comenzi și funcții auxiliare
 
 # Instanțiați și adăugați extensia la bot
