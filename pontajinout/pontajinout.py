f93516debbb1b2ff3f42c5dead52c7785934f2ea
Ursu17 <138593499+Ursu17@users.noreply.github.com>
2023-08-30 17:41:55 +0300

    Update pontajinout.py

--- pontajinout/pontajinout.py
+++ pontajinout/pontajinout.py
@@ -27,6 +27,12 @@ class PontajInOut(commands.Cog):
         if channel is not None:
             await channel.send(content)
 
+    def serialize_datetime(self, dt):
+        return dt.strftime('%Y-%m-%d %H:%M:%S')
+
+    def deserialize_datetime(self, dt_str):
+        return datetime.strptime(dt_str, '%Y-%m-%d %H:%M:%S')
+
     @commands.group()
     async def pontaj(self, ctx):
         """Comenzi pentru înregistrarea pontajului de intrare/ieșire"""
@@ -37,7 +43,7 @@ class PontajInOut(commands.Cog):
         """Înregistrează pontajul de intrare"""
         guild_settings = await self.config.guild(ctx.guild).all()
         user_pontaje = guild_settings["pontaje"].get(ctx.author.id, [])
-        user_pontaje.append(datetime.now(self.bucharest_tz))
+        user_pontaje.append(self.serialize_datetime(datetime.now(self.bucharest_tz)))
         await self.config.guild(ctx.guild).set_raw("pontaje", ctx.author.id, value=user_pontaje)
 
         await ctx.message.delete()
@@ -49,50 +55,11 @@ class PontajInOut(commands.Cog):
 
         if pontaj_in_channel:
             await self.post_message(pontaj_in_channel,
-                                    f"{ctx.author.mention} a înregistrat pontajul de intrare: {user_pontaje[-1].strftime('%H:%M')}")
+                                    f"{ctx.author.mention} a înregistrat pontajul de intrare: {user_pontaje[-1]}")
         else:
             await ctx.send("Canalul pentru înregistrarea pontajului de intrare nu este configurat sau nu există.")
 
-    @pontaj.command(name="out")
-    async def pontaj_out(self, ctx):
-        """Înregistrează pontajul de ieșire și calculează durata de lucru"""
-        guild_settings = await self.config.guild(ctx.guild).all()
-        user_pontaje = guild_settings["pontaje"].get(ctx.author.id, [])
-
-        if not user_pontaje:
-            await ctx.send("Folosește mai întâi **!pontaj in**.")
-            return
-        
-        work_duration = datetime.now(self.bucharest_tz) - user_pontaje[-1]
-        work_minutes = int(work_duration.total_seconds() / 60)
-        
-        await ctx.message.delete()
-        await self.delete_command(ctx)
-        
-        pontaj_out_time = datetime.now(self.bucharest_tz)
-
-        if len(user_pontaje) > 1:
-            user_pontaje[-1] = pontaj_out_time
-            guild_settings["pontaje"][ctx.author.id] = user_pontaje
-            await self.config.guild(ctx.guild).set(guild_settings)
-
-            guild_settings = await self.config.guild(ctx.guild).all()
-            pontaj_out_channel_id = guild_settings["pontaj_out_channel"]
-            pontaj_out_channel = ctx.guild.get_channel(pontaj_out_channel_id)
-
-            if pontaj_out_channel:
-                await self.post_message(pontaj_out_channel,
-                                        f"{ctx.author.mention} a înregistrat pontajul de ieșire: {pontaj_out_time.strftime('%H:%M')} "
-                                        f"(A stat în tură {work_minutes} de minute)")
-            else:
-                await ctx.send("Canalul pentru înregistrarea pontajului de ieșire nu este configurat sau nu există.")
-        else:
-            await ctx.send("Folosește mai întâi **!pontaj in**.")
-
     # Restul codului pentru comenzi și funcții auxiliare
-
-    async def delete_command(self, ctx):
-        await ctx.message.delete(delay=3)  # Șterge mesajul după 3 secunde
 
 # Instanțiați și adăugați extensia la bot
 def setup(bot):
