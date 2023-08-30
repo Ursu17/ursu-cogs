68a3be042565d3942aa9b1c7d01955640efbe752
Ursu17 <138593499+Ursu17@users.noreply.github.com>
2023-08-30 15:48:54 +0300

    Update clockinout.py

--- clockinout/clockinout.py
+++ clockinout/clockinout.py
@@ -19,17 +19,17 @@ class ClockInOut(commands.Cog):
         self.config.register_guild(**default_guild)
         self.clock_in_time = None
 
-    @commands.command()
+    @commands.command(name="in")
     async def clock_in(self, ctx):
         """Înregistrează intrarea la serviciu"""
         self.clock_in_time = datetime.now(timezone.utc)
-        await ctx.send(f"{ctx.author.mention} a înregistrat ora de intrare: {self.clock_in_time.strftime('%H:%M')}")
+        await ctx.send(f"{ctx.author.mention} a intrat în tură la : {self.clock_in_time.strftime('%H:%M')}")
 
-    @commands.command()
+    @commands.command(name="out")
     async def clock_out(self, ctx):
         """Înregistrează ieșirea de la serviciu și calculează diferența de timp"""
         if not hasattr(self, "clock_in_time") or self.clock_in_time is None:
-            await ctx.send("Nu ai înregistrat încă ora de intrare.")
+            await ctx.send("Scrie mai intai !uin.")
             return
         
         clock_out_time = datetime.now(timezone.utc)
@@ -37,15 +37,15 @@ class ClockInOut(commands.Cog):
         elapsed_minutes = int(elapsed_time.total_seconds() / 60)
         self.clock_in_time = None
         
-        await ctx.send(f"{ctx.author.mention} a înregistrat ora de ieșire: {clock_out_time.strftime('%H:%M')} "
+        await ctx.send(f"{ctx.author.mention} a ieșit din tura la: {clock_out_time.strftime('%H:%M')} "
                        f"({elapsed_minutes} minute)")
 
-    @commands.command(name="setchannel")
-    async def set_channel(self, ctx, event: str, channel: discord.TextChannel):
+    @commands.command(name="clocksetchannel")
+    async def clock_set_channel(self, ctx, event: str, channel: discord.TextChannel):
         """Configurează canalul pentru evenimentul specificat (intrare/ieșire)"""
-        # Restul codului set_channel
+        # Restul codului clock_set_channel
 
-    @commands.command(name="resetchannel")
-    async def reset_channel(self, ctx, event: str):
+    @commands.command(name="clockresetchannel")
+    async def clock_reset_channel(self, ctx, event: str):
         """Resetează canalul pentru evenimentul specificat (intrare/ieșire)"""
-        # Restul codului reset_channel
+        # Restul codului clock_reset_channel
